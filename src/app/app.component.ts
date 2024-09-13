cd5b41aed9081d87977158bfba6c87291dde9be1
namansaini1463 <namansaini1463@gmail.com>
2024-09-13 15:58:12 +0530

    Deleting a task works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
+
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+  selectedUserId: string | undefined;
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
+
+  onSelectUser(id: string) {
+    // console.log('Selected the user with the id: ' + id);
+    this.selectedUserId = id;
+  }
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,5 @@
-import { Component } from '@angular/core';
-
-import { DUMMY_USERS } from '../dummy-users';
-
-const getRandom = () => {
-  return Math.floor(Math.random() * DUMMY_USERS.length);
-};
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { User } from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -14,13 +9,60 @@ const getRandom = () => {
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[getRandom()];
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected: boolean | undefined;
+
+  @Output() selectUser = new EventEmitter<string>();
 
   get userImagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
-  onSelectUser = () => {
-    this.selectedUser = DUMMY_USERS[getRandom()];
-  };
+  onSelectUser() {
+    this.selectUser.emit(this.user.id);
+  }
+
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  // selectUser = output<string>();
+
+  // userImagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // });
+
+  // onSelectUser() {
+  //   this.selectUser.emit(this.id);
+  // }
 }
+
+// import { Component, computed, signal } from '@angular/core';
+
+// import { DUMMY_USERS } from '../dummy-users';
+
+// // const getRandom = () => {
+// //   return Math.floor(Math.random() * DUMMY_USERS.length);
+// // };
+
+// @Component({
+//   selector: 'app-user',
+//   standalone: true,
+//   imports: [],
+//   templateUrl: './user.component.html',
+//   styleUrl: './user.component.css',
+// })
+// // export class UserComponent {
+// //   selectedUser = signal(DUMMY_USERS[getRandom()]);
+// //   userImagePath = computed(() => 'assets/users/' + this.selectedUser().avatar); // This is used when the concept of signals is used
+
+// //   // get userImagePath() {
+// //   //   return 'assets/users/' + this.selectedUser.avatar;
+// //   // }
+
+// //   onSelectUser = () => {
+// //     this.selectedUser.set(DUMMY_USERS[getRandom()]);
+// //   };
+// export class UserComponent {}
